bf84fbf6ffba6c0d49fc658566223035279eab46
Richard Loera <richexp9@gmx.com>
2017-11-22 19:56:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,8 @@
 function forLoop(array) {
   var loopyArray = []
   for (var i = 0; i < 25; i++) {
-    array[i]= "I am ${i} strange loops."
+    loopyArray[i]= "I am ${i} strange loops."
   }
-  
 }
 
 console.log(array)
