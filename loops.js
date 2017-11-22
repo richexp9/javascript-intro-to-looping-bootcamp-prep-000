2458dd46eaf95d14200a4e66e59e202d2f7b428c
Richard Loera <richexp9@gmx.com>
2017-11-22 20:11:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,24 +1,24 @@
 var loopyArray = []
 
-function forLoop(array) {
-  for (var i = 0; i < 25; i++) {
-    array.push(`I am ${i} strange loops.`)
-  }
-  return array
-}
-
-//console.log(forLoop(loopyArray))
-
-
-function whileLoop(number) {
-  while (number > 0) {
-    console.log(--number)
-  }
-  return 'done'
-}
-
-//whileLoop(100)
-
+// function forLoop(array) {
+//   for (var i = 0; i < 25; i++) {
+//     array.push(`I am ${i} strange loops.`)
+//   }
+//   return array
+// }
+// 
+// //console.log(forLoop(loopyArray))
+// 
+// 
+// function whileLoop(number) {
+//   while (number > 0) {
+//     console.log(--number)
+//   }
+//   return 'done'
+// }
+// 
+// //whileLoop(100)
+// 
 
 
 function maybeTrue() {
